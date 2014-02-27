f05177ef2cd49d4e8c946950f82f14004845a494
Martin Unzner <abmelden22@gmx.de>
2014-02-27 11:37:33 +0100

    netperf should measure CPU utilisation

--- start-netperf.sh
+++ start-netperf.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 if [ "x$1" = "x" ]; then
+  echo "Please specify a target host."
+  exit
+fi
+if [ "x$2" = "x" ]; then
   echo "Please specify a target directory."
   exit
 fi
-mkdir ~/stats/$1 > /dev/null 2>&1
+mkdir -p $2 > /dev/null 2>&1
 for i in tcp_stream tcp_maerts udp_stream; do
-  bin/netperf -H 192.168.0.1 -t $i -- -o > ~/stats/$1/$i.csv;
+  bin/netperf -cCH $1 -t $i -- -o > $2/$i.csv;
 done
-for i in tcp_rr tcp_cc tcp_crr udp_rr; do
-  bin/netperf -H 192.168.0.1 -l -1000000 -t $i -- -o > ~/stats/$1/$i.csv;
+for i in tcp_rr tcp_crr udp_rr; do
+  bin/netperf -cCH $1 -l -1000000 -t $i -- -o > $2/$i.csv;
 done
 
